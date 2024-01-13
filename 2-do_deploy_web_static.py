f382c123ad593466e143132511e8178ee8677845
M0hammed-Gamal <m_m20000075@yahoo.com>
2024-01-13 03:03:48 -0800

    hot fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["54.157.152.98", "54.236.46.122"]
 env.user = "ubuntu"
 
 
