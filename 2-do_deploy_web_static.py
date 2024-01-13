bcf05c7cfe2a9072cc6774e2603a0450e49c6cca
M0hammed-Gamal <m_m20000075@yahoo.com>
2024-01-13 22:12:48 +0000

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,40 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
+Invoke script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import put, run, env
+from invoke import task
 from os.path import exists
-env.hosts = ['54.157.152.98', '54.236.46.122']
 
-def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+@task
+def do_deploy(c, archive_path):
+    """Distributes an archive to the web servers"""
+    if not exists(archive_path):
         return False
+
     try:
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        
+        # Upload the archive
+        c.put(archive_path, "/tmp/")
+
+        # Create the necessary directories and extract the archive
+        c.run('mkdir -p {}{}/'.format(path, no_ext))
+        c.run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+
+        # Clean up
+        c.run('rm /tmp/{}'.format(file_n))
+        c.run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        c.run('rm -rf {}{}/web_static'.format(path, no_ext))
+        c.run('rm -rf /data/web_static/current')
+        c.run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+
         return True
-    except:
+    except Exception as e:
+        print("Deployment failed:", str(e))
         return False
+
