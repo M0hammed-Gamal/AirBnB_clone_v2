35be7e21e53cdb18efe6684fcb2dc02f3bfa61fd
M0hammed-Gamal <m_m20000075@yahoo.com>
2024-01-13 22:46:39 +0000

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,56 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
 """
-Invoke script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Invoke script that distributes an archive to your web servers
 """
 
+from datetime import datetime
 from invoke import task
-from os.path import exists
+import os
+
+hosts = ["52.91.121.146", "3.85.136.181"]
+user = "ubuntu"
+
 
 @task
-def do_deploy(c, archive_path):
-    """Distributes an archive to the web servers"""
-    if not exists(archive_path):
-        return False
+def do_pack(ctx):
+    """
+    Return the archive path if archive has generated correctly.
+    """
+    ctx.run("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = ctx.run("tar -cvzf {} web_static".format(archived_f_path))
 
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        
-        # Upload the archive
-        c.put(archive_path, "/tmp/")
+    if t_gzip_archive.ok:
+        return archived_f_path
+    else:
+        return None
 
-        # Create the necessary directories and extract the archive
-        c.run('mkdir -p {}{}/'.format(path, no_ext))
-        c.run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
 
-        # Clean up
-        c.run('rm /tmp/{}'.format(file_n))
-        c.run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        c.run('rm -rf {}{}/web_static'.format(path, no_ext))
-        c.run('rm -rf /data/web_static/current')
-        c.run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+@task
+def do_deploy(ctx, archive_path):
+    """
+    Distribute archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        ctx.run("sudo mkdir -p {}".format(newest_version))
+        ctx.run("sudo tar -xzf {} -C {}/".format(archived_file, newest_version))
+        ctx.run("sudo rm {}".format(archived_file))
+        ctx.run("sudo mv {}/web_static/* {}".format(newest_version, newest_version))
+        ctx.run("sudo rm -rf {}/web_static".format(newest_version))
+        ctx.run("sudo rm -rf /data/web_static/current")
+        ctx.run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
+        print("New version deployed!")
         return True
-    except Exception as e:
-        print("Deployment failed:", str(e))
+
+    else:
+        print("Error: Archive not found at {}".format(archive_path))
         return False
+
+# Uncomment the line below to test the deployment
+# do_deploy(do_pack())
 
