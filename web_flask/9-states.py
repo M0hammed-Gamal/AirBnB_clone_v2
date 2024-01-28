a2e3c0e93b53de31626445bdf7f4a91b2af88dd2
M0hammed-Gamal <m_m20000075@yahoo.com>
2024-01-28 19:28:59 +0000

    fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
-from flask import Flask, render_template
-from models import storage
-from models.state import State
-
+""" a script that starts a Flask web application """
+from flask import Flask
+from flask import render_template
+from models import storage, State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close(self):
-    """ Method to close the session """
+def remove_session(exception):
+    """ After each request, it removes the current SQLAlchemy Session """
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
-def state():
-    """Displays a html page with states"""
-    states = storage.all(State)
-    return render_template('9-states.html', states=states, mode='all')
+def render_states():
+    """ displays all states """
+    States = storage.all(State).values()
+    return render_template("9-states.html", States=States, one=None)
 
 
-@app.route('/states/<id>', strict_slashes=False)
-def state_by_id(id):
-    """Displays a html page with citys of that state"""
-    for state in storage.all(State).values():
-        if state.id == id:
-            return render_template('9-states.html', states=state, mode='id')
-    return render_template('9-states.html', states=state, mode='none')
-
+@app.route('/states/<string:id>', strict_slashes=False)
+def render_one_state(id):
+    """ displays one state if it exists """
+    key = "State." + id
+    one = None
+    if key in storage.all(State):
+        one = storage.all(State)[key]
+    return render_template("9-states.html", States=None, one=one)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host='0.0.0.0', port=5000)
